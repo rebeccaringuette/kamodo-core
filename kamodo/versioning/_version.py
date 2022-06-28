bac6df1c5f08434c04d39a17fc59f1cbc94ed83d
Asher Pembroke <apembroke@gmail.com>
2022-06-28 12:04:34 -0500

    stable release

--- kamodo/versioning/_version.py
+++ kamodo/versioning/_version.py
@@ -7,5 +7,5 @@ Provides versioning version information.
 
 from incremental import Version
 
-__version__ = Version("versioning", 22, 6, 0, release_candidate=1)
+__version__ = Version("versioning", 22, 6, 0)
 __all__ = ["__version__"]

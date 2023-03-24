2f8069190558176b41c60c5278dffdfd1762b9de
rebeccaringuette <49281118+rebeccaringuette@users.noreply.github.com>
2023-03-24 11:53:12 -0400

    typo fix

--- kamodo/util.py
+++ kamodo/util.py
@@ -61,8 +61,8 @@ unit_subs = dict(
     cc=sympy_units.cm ** 3,
     AU=get_unit_quantity('astronomical unit', 'm', 1.496e+11, 'AU', 'SI'),
     arcsec=get_unit_quantity('arc seconds', 'degrees', 1. / 3600, '\"', 'SI'),
-    hr=get_unit_quantity('hour', 's', 3600., 'hr', 'SI')
-    TECU = 10**16 / sympy_units.m ** 2   # get_unit_quantity('TECU','1/m**2',10**16, 'TECU','SI')
+    hr=get_unit_quantity('hour', 's', 3600., 'hr', 'SI'),
+    TECU=10**16 / sympy_units.m ** 2   # get_unit_quantity('TECU','1/m**2',10**16, 'TECU','SI')
 )
 
 sympy_units.erg = unit_subs['erg']
